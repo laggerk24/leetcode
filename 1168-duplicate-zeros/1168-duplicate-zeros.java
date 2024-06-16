aaaaedd81bf385108d8cb39e07bff1ec6e3dedf5
laggerk24 <53289529+laggerk24@users.noreply.github.com>
2024-06-17 01:53:55 +0530

    Time: 1 ms (97.36%), Space: 44.3 MB (89.48%) - LeetHub

--- 1168-duplicate-zeros/1168-duplicate-zeros.java
+++ 1168-duplicate-zeros/1168-duplicate-zeros.java
@@ -1,11 +1,27 @@
 class Solution {
     public void duplicateZeros(int[] arr) {
-        for(int i = 0;i<arr.length;i++){
-            if(arr[i] == 0){
-                for(int j=arr.length-1;j>i;j--){
-                    arr[j] = arr[j-1];
-                }
-                i++;
+        int n = arr.length;
+        int[] copyArray = new int[n];
+        int index = 0;
+        boolean flag = true;
+        for(int i=0;i<n;i++){
+            if(arr[i] == 0 && flag){
+                index = i;
+                flag = false;
+            } 
+            copyArray[i] = arr[i];
+        }
+        index++;
+        if(index<n && !flag) arr[index] = 0;
+        int secondIndex = index;
+        index++;
+        while(index < n && secondIndex < n && !flag){
+            if(copyArray[secondIndex] != 0){
+                arr[index++] = copyArray[secondIndex++];
+            }
+            else if(copyArray[secondIndex] == 0){
+                arr[index++] = copyArray[secondIndex++];
+                if(index<n) arr[index++] = 0;
             }
         }
     }
