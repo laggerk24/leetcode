0dea71592d3e0eb4fd03623fea4bdca43c910a85
laggerk24 <53289529+laggerk24@users.noreply.github.com>
2024-06-28 20:43:55 +0530

    Time: 7 ms (87.57%), Space: 62.7 MB (71.35%) - LeetHub

--- 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.java
+++ 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.java
@@ -5,23 +5,13 @@ class Solution {
             freq[road[0]]++;
             freq[road[1]]++;
         }
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->b[1]-a[1]);
-        for(int i=0;i<n;i++){
-            pq.offer(new int[]{i,freq[i]});
-        }
-        int count = n;
-        while(!pq.isEmpty()){
-            int[] road = pq.poll();
-            freq[road[0]] = count;
-            count--;
-        }
+        Arrays.sort(freq);
         long ans = 0;
-        for(int[] road:roads){
-            ans = ans + freq[road[0]]+freq[road[1]];
+        long count = 1;
+        for(int i=0;i<n;i++){
+            ans += (freq[i]*count);
+            count++;
         }
-        // for(int i=0;i<n;i++){
-        //     System.out.println(i+" - "+freq[i]);
-        // }
         return ans;
     }
 }
