2eb4ad5aac3e2be022e72c8f5a0b3d9792938b8c
laggerk24 <53289529+laggerk24@users.noreply.github.com>
2024-09-01 00:14:39 +0530

    Time: 47 ms (5.43%), Space: 55.1 MB (34.26%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.java
@@ -1,17 +1,18 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
-        int n = nums.length;
-        HashMap<Integer,Integer> mpp = new HashMap<>();
+        int oddCount = 0;
+        int evenCount = 0;
+        HashMap<Integer,Integer> map = new HashMap<>();
         int sum = 0;
-        int ans =0;
-        mpp.put(0,1);
-        for(int i=0;i<n;i++){
-            if(nums[i]%2==1){
-                sum+=1;
+        int ans = 0;
+        map.put(0,1);
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]%2 == 1){
+                sum += 1;
             }
             int diff = sum-k;
-            ans += mpp.getOrDefault(diff,0);
-            mpp.put(sum,mpp.getOrDefault(sum,0)+1);
+            ans += map.getOrDefault(diff,0);
+            map.put(sum,map.getOrDefault(sum,0)+1);
         }
         return ans;
     }
